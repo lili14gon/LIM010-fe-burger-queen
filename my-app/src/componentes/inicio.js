4d11b01e3ca3f2f20eafc3064abfa796eb81d091
Betsycaceres <betsy0357@gmail.com>
2019-10-10 18:18:30 -0500

    creando componentes

---
+++ my-app/src/componentes/inicio.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Inicio() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    <button>MESERO</button>
+    <button>JEFE DE COCINA</button>
+    </div>
+  );
+}
+export default Inicio;
---
+++ my-app/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+// import  Inicio from './componentes/inicio';
+import PedidoMesero from './componentes/mesero';
+import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDCm5JWgQ3Vm4X2a7Q7ZCz6aqOp1gHcTzI",
+    authDomain: "burger-queen-react-bls.firebaseapp.com",
+    databaseURL: "https://burger-queen-react-bls.firebaseio.com",
+    projectId: "burger-queen-react-bls",
+    storageBucket: "burger-queen-react-bls.appspot.com",
+    messagingSenderId: "658478814346",
+    appId: "1:658478814346:web:f4bf5219e89878fd114973"
+  };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+ReactDOM.render(<PedidoMesero />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
