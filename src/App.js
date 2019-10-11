bb3545dea4d8293340fdf69764bfdeeefa339427
Betsycaceres <betsy0357@gmail.com>
2019-10-10 20:57:34 -0500

    definiendo rutas Home, mesero, feje de cocina

--- src/App.js
+++ src/App.js
@@ -1,3 +1,52 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './componentes/inicio';
+import About from './componentes/mesero';
+import Users from './componentes/jefecocina';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">Mesero</Link>
+            </li>
+            <li>
+              <Link to="/users">Jefe de Cocina</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+
+
 // import React from 'react';
 // import logo from './logo.svg';
 // import './App.scss';
@@ -23,59 +72,3 @@
 //   );
 // }
 // export default App;
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
-
--- src/componentes/inicio.js
+++ src/componentes/inicio.js
@@ -1,12 +1,23 @@
 import React from 'react';
-function Inicio() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    <button>MESERO</button>
-    <button>JEFE DE COCINA</button>
-    </div>
-  );
-}
-export default Inicio;
+function Home() {
+    return (
+      <div> 
+      <button>MESERO</button>
+      <button>JEFE DE COCINA</button>
+      </div>
+    );
+  }
+  export default Home;
+  
+  // import React from 'react';
+// function Inicio() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//       </header>
+//     <button>MESERO</button>
+//     <button>JEFE DE COCINA</button>
+//     </div>
+//   );
+// }
+// export default Inicio;
