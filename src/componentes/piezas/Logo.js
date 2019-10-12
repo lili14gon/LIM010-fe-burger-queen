8d1b0c36ff4cf6690011c177bf80200131810410
lili14gon <liligh14@gmail.com>
2019-10-11 19:06:46 -0500

    subiendo imagen

--- src/componentes/piezas/Logo.js
+++ src/componentes/piezas/Logo.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import logo from '../image/burguer.png';
 
 function Logo() {
   return (
         <div>
-      
+      <img src={logo} alt="logo"/>
         </div>
       );
   }
