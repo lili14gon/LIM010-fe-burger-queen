ecd5890682e3ceac0e32830746c391bd1c2f87f8
lili14gon <liligh14@gmail.com>
2019-10-17 15:21:48 -0500

    cambios

--- src/componentes/mesero.js
+++ src/componentes/mesero.js
@@ -2,11 +2,22 @@ import React, { useState, useEffect } from 'react';
 import firebase from './firebase';
 import '../App.css'
 
+function subMenu() {
+  return(
+    <div>
+     <button type="button">HAMBURGUESA</button>
+     <button type="button">ACOMPAÑAMIENTO</button>
+     <button type="button">BEBIDAS</button>
+   </div>
+  );
+}
+
 function About() {
   const [name,setName] = useState('');
   const [tipo, setTipo] = useState('');
   const [productos, setProductos] = useState([]);
   // const [categorias, setCategorias] = useState([]);
+  // const menu = subMenu();
 
   const getProductos = () => {
     firebase.firestore().collection("productos").where('tipo', '==', tipo).get().then(function (dato) {
@@ -23,6 +34,46 @@ function About() {
     setName(e.target.value);
   }
 
+
+  // return (
+  //   <div className="App-header">
+  //     <form>
+  //       <label>NOMBRE DEL CLIENTE
+  //               {/* <input type="text" /> */}
+  //               <input type="text"
+  //               value={name}
+  //               onChange = {handleNamChange}
+  //               />
+  //       </label>
+  //   //    <p>{name}</p>
+  //       <div>
+  //         <p>{name}</p>
+  //       </div>
+  //       <div>
+  //         <button type="button" onClick={() => { setTipo('desayuno') }}>DESAYUNO</button>
+  //         <button type="button" id="almuerzo" onClick={(e) => { setTipo('almuerzo') }}>ALMUERZO  Y CENA</button>
+  //       </div>
+  //       <input type="submit" />
+  //     </form>
+      
+  //     {document.getElementById = "desayuno" ?
+  //       <div>
+  //       <button type="button">HAMBURGUESA</button>
+  //       <button type="button">ACOMPAÑAMIENTO</button>
+  //       <button type="button">BEBIDAS</button>
+  //     </div>
+  //       :
+  //       null
+  //     }
+  //     <ul>
+  //       {productos.map((p) => (
+  //         <li>{p.nombre}</li>
+  //       ))}
+  //     </ul>
+
+  //   </div>
+  // );
+
   return (
     <div className="App-header">
       <form>
@@ -33,40 +84,28 @@ function About() {
                 onChange = {handleNamChange}
                 />
         </label>
-        <p>{name}</p>
+        {/* <p>{name}</p> */}
         <div>
           <p>{name}</p>
         </div>
         <div>
           <button type="button" onClick={() => { setTipo('desayuno') }}>DESAYUNO</button>
-          <button type="button" id="almuerzo" onClick={(e) => { setTipo('almuerzo') }}>ALMUERZO  Y CENA</button>
+          <button type="button" id="almuerzo" onClick={ () => {subMenu()} }>ALMUERZO  Y CENA</button>
         </div>
-        <input type="submit" />
+        {/* <input type="submit" /> */}
       </form>
-      
-      {document.getElementById = "desayuno" ?
-        <div>
-        <button type="button">HAMBURGUESA</button>
-        <button type="button">ACOMPAÑAMIENTO</button>
-        <button type="button">BEBIDAS</button>
-      </div>
-        :
-        null
-      }
       <ul>
         {productos.map((p) => (
-          <li>{p.nombre}</li>
+          <li>{p.nombre} {p.precio}</li>
         ))}
       </ul>
 
     </div>
   );
+
 }
 
-// const CategoriaAlmuerzo =()=>{
-//   return(
 
-//   );
-// }
+
 
 export default About;
