8561cabb6b2ca9707d1b36b11c1c0d33a3106d04
Betsycaceres <betsy0357@gmail.com>
2019-10-28 15:55:04 -0500

    agregando estado al componente  nueva orden

--- src/componentes/Jefe de Cocina/JefeCocina.js
+++ src/componentes/Jefe de Cocina/JefeCocina.js
@@ -2,14 +2,12 @@ import React from 'react';
 import Header from '../piezas/Header';
 import Footer from '../piezas/Footer';
 import PedidosCocina from './PedidosCocina';
-import NavBar from '../piezas/NavBar';
 import TituloPedidos from '../piezas/TituloPedidos';
 
 const Cocina = () => {
   return (
     <React.Fragment>
       <Header />
-      <NavBar />
       <div>
         <TituloPedidos texto="Pedidos"/>
         <PedidosCocina />
--- src/componentes/piezas/Header.js
+++ src/componentes/piezas/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Logo from './Logo';
 import '../../css/App.css'
-function Header() { 
+
+import { Link } from "react-router-dom";
+function Header() {
   return (
     <header className=" row even center-item">
-      <p className="">Burguer Queen</p>
-      <Logo></Logo>
+      <div>  <p className="">Burguer Queen</p>
+      </div>
+      <div> <Logo></Logo></div>
+      <div className="row">
+        <button type="button" className="btn"><Link to="/about">Mesero</Link></button>
+        <button type="button" className="btn"><Link to="/cocina">Jefe de Cocina</Link></button>
+        <button type="button" className="btn"><Link to="/despacho">Listo para servir</Link></button>
+        <button type="button" className="btn"><Link to="/finalizados">Entregados</Link></button>
+      </div>
     </header>
   );
 }
